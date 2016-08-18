7cfc68db1231e22c50b18616b4c9c28309857f66
SmilinSamurai <cbrown8100@gmail.com>
2016-08-18 11:26:29 +0100

    Added comments to Pangrams project

--- Algorithms/Strings/Pangrams/main.cpp
+++ Algorithms/Strings/Pangrams/main.cpp
@@ -10,22 +10,22 @@ using namespace std;
 
 int main() {
     string s;
-    getline(cin, s);
-    s.erase(::remove_if(s.begin(), s.end(), ::isspace), s.end());
+    getline(cin, s); // Read in the string
+    s.erase(::remove_if(s.begin(), s.end(), ::isspace), s.end()); // Remove spaces for the string
     for(int i = 0; i < s.size(); i++) {
-        if (isupper(s[i])) {
-            s[i] = tolower(s[i]);
+        if (isupper(s[i])) { // check for uppercase letters
+            s[i] = tolower(s[i]); // if there is an upper case, than change it to lower case
         }
     }
-    sort(s.begin(), s.end());
+    sort(s.begin(), s.end()); // sort the string
     string abc= "";
     for (char i = 'a'; i <= 'z'; i++) {
-        abc += i;
+        abc += i; // adding the alphabet to the empty string
     }
     int counter = 0;
     for (int i = 0; i < abc.size(); i++) {
         for (int j = 0; j < s.size(); j++) {
-            if (abc[i] == s[j]) {
+            if (abc[i] == s[j]) { // compares the string against the alphabet
                 ++i;
                 counter++;
             }
